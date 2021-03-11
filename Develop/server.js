8d0cd6a679a6e54320adfd47cb0741e91037ab49
Greg Harris <ghart.productions@gmail.com>
2021-03-10 18:06:31 -0800

    added dependencies & PORT listener

---
+++ Develop/server.js
@@ -0,0 +1,22 @@
+// Dependencies
+const express = require('express');
+const app = express();
+
+const PORT = 8000
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// PORT Listener
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
